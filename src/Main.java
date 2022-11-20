2e6dfad443e85d6a26eeb971c76d13c2c8187e7f
nellya <nellya.ziganshina@gmail.com>
2022-11-20 17:29:44 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+        //task 1
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("поздравить с совершеннолетием");
+
+
+
+            //task 2
+            int schoolAge = 7;
+            int universityAge = 18;
+            int workingAge = 24;
+            if (schoolAge >= 7) {
+                System.out.println("ребенок ходит в школу");
+                if (universityAge >= 18) {
+                    System.out.println("человек может отправляться в университет");
+                    if (workingAge >= 24) {
+                        System.out.println("человеку пора искать первую работу");
+
+                    }
+
+                }
+            }
+            //task 3 не получается решить
+
+
+                // task 4
+            age = 18;
+                if (age >= 18) {
+                    System.out.println("поздравить с совершеннолетием");
+                }else{
+                    System.out.println("возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+                }
+
+
+
+                }
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
